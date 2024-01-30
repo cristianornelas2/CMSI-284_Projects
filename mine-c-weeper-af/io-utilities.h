2b04014bd0697082c1b038c22d24ed9e626df6b9
Cristian Ornelas <46763991+cristianornelas2@users.noreply.github.com>
2024-01-29 22:21:39 -0800

    Delete mine-c-weeper-af directory

--- mine-c-weeper-af/io-utilities.h
+++
@@ -1,9 +0,0 @@
-#ifndef _IO_UTILITIES_H
-#define _IO_UTILITIES_H
-
-/**
- * Helper function for sending code points > 0x7F to a UTF-8 capable terminal.
- */
-void emit_utf_8(int code_point);
-
-#endif
